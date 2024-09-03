e5d9cb45075780f2bf947ec1bcd215f0bfd2f543
WIN-LIN <puppy95176@gmail.com>
2024-09-03 09:55:27 +0800

    fix: Module parse failed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "imaccount-sdk": path.resolve(__dirname, "node_modules/imaccount-sdk"),
+      "~packages": path.resolve(
+        __dirname,
+        "node_modules/imaccount-sdk/packages"
+      ),
+      // "~": path.resolve(__dirname, "node_modules/imaccount-sdk"),
+    };
+    config.module.rules.push({
+      test: /\.(ts)x?$/,
+      use: [
+        {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            experimentalWatchApi: true,
+            onlyCompileBundledFiles: true,
+            configFile: "tsconfig.json",
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
